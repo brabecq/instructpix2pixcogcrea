34ae44236abd05170efb26c98cadb4313c0c1e3e
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-05-23 15:58:45 +0200

    Multi GPU support

--- predict.py
+++ predict.py
@@ -1,35 +1,29 @@
 from cog import BasePredictor, Input, Path
 import torch
 from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
-import PIL
-import requests
 from typing import Any
-import base64
-from io import BytesIO
+
+import torch.multiprocessing as mp
+from .run_distributed import run_inference
 
 model_id = "timbrooks/instruct-pix2pix"
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        self.pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None).to("cuda")
+        self.pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
         self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
+        world_size = torch.cuda.device_count()
+        mp.spawn(
+            self.predict,
+            args=(world_size,),
+            nprocs=world_size,
+            join=False,
+        )
+        self.world_size = world_size
 
     # The arguments and types the model takes as input
-    def predict(self, prompt:str="", image_url:str="", num_inference_steps:int=100, image_guidance_scale:float=20, guidance_scale:float=7.0) -> Any:
+    def predict(self, prompt:str="", image_url:str="", num_inference_steps:int=100, image_guidance_scale:float=20, guidance_scale:float=7.0, gpu_index=0) -> Any:
         """Run a single prediction on the model"""
-        image = download_image(image_url)
-        images = self.pipe(prompt=prompt, num_inference_steps=num_inference_steps, image_guidance_scale=image_guidance_scale, guidance_scale=guidance_scale, image=image).images
-        output = images[0]
-        im_file = BytesIO()
-        output.save(im_file, format="PNG")
-        im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
-        return base64.b64encode(im_bytes)
-
-
-def download_image(url):
-    image = PIL.Image.open(requests.get(url, stream=True).raw)
-    image = PIL.ImageOps.exif_transpose(image)
-    image = image.convert("RGB")
-    return image 
-
+        # create default process group
+        return run_inference(gpu_index, self.world_size, prompt, image_url, num_inference_steps, image_guidance_scale, guidance_scale, self.pipe)
