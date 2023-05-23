8944284f3fe45278956acf612aa8f3701d907c4d
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-05-23 14:17:50 +0200

    Multi GPU support

--- predict.py
+++ predict.py
@@ -14,19 +14,21 @@ class Predictor(BasePredictor):
         # Define the device to run the model on
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         """Load the model into memory to make running multiple predictions efficient"""
-        model_id = "timbrooks/instruct-pix2pix"
-        pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16,
-                                                                           safety_checker=None)
-        if torch.cuda.device_count() > 1:
-            pipe = DataParallel(pipe)
-        self.pipe = pipe.to(self.device)
-        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
+        # model_id = "timbrooks/instruct-pix2pix"
 
-    # The arguments and types the model takes as input
+        model = torch.load("checkpoints/instruct-pix2pix-00-22000.ckpt")
+        # pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16,
+        #                                                                    safety_checker=None)
+        if torch.cuda.device_count() > 1:
+            model = DataParallel(model)
+        self.model = model.to(self.device)
+        # self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
+
+    @torch.inference_mode()
     def predict(self, prompt:str="", image_url:str="", num_inference_steps:int=100, image_guidance_scale:float=20, guidance_scale:float=7.0) -> Any:
         """Run a single prediction on the model"""
         image = download_image(image_url)
-        images = self.pipe(prompt=prompt, num_inference_steps=num_inference_steps, image_guidance_scale=image_guidance_scale, guidance_scale=guidance_scale, image=image).images
+        images = self.model(prompt=prompt, num_inference_steps=num_inference_steps, image_guidance_scale=image_guidance_scale, guidance_scale=guidance_scale, image=image).images
         output = images[0]
         im_file = BytesIO()
         output.save(im_file, format="PNG")
