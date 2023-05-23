089a14a73b1721562f709d759de0575857f1ab09
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-05-23 16:49:47 +0200

    Change imports

--- predict.py
+++ predict.py
@@ -8,6 +8,11 @@ from run_distributed import run_inference
 
 model_id = "timbrooks/instruct-pix2pix"
 
+def predict(prompt:str="", image_url:str="", num_inference_steps:int=100, image_guidance_scale:float=20, guidance_scale:float=7.0, gpu_index:int=0, predictor) -> Any:
+    """Run a single prediction on the model"""
+    # create default process group
+    predictor.predict(prompt, image_url, num_inference_steps, image_guidance_scale, guidance_scale, gpu_index)
+
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
