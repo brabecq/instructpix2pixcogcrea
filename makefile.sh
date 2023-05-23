2ece27da4137f0345c356205ffaee980467c77b5
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-05-23 16:01:45 +0200

    Multi GPU support

--- makefile.sh
+++ makefile.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-mkdir -p $SCRIPT_DIR/../checkpoints
-
-curl http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt -o $SCRIPT_DIR/../checkpoints/instruct-pix2pix-00-22000.ckpt
+sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`
+sudo chmod +x /usr/local/bin/cog
 
 sudo cog build -t instructpix2pixcogcrea
 sudo docker run -d -p 5000:5000 --gpus all instructpix2pixcogcrea
