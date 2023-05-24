43a74bad6c1ddd3764a45a965ffcc07846a18cd1
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-05-24 16:21:21 +0200

    Back to multiprocessing

--- run_inference.py
+++ run_inference.py
@@ -21,7 +21,7 @@ def run_inference(rank,
     im_file = BytesIO()
     output.save(im_file, format="PNG")
     im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
-    requests.post(f"http://localhost:8000/{rank}", json={"img": base64.b64encode(im_bytes).decode("utf-8")}
+    requests.post(f"http://localhost:8000/{rank}", json={"img": base64.b64encode(im_bytes).decode("utf-8")})
     # return {"img": base64.b64encode(im_bytes)}
 
 
