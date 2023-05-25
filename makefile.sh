a374778d98bdb15e92ba430af1c73d71dfb19d38
brabecq <92429709+brabecq@users.noreply.github.com>
2023-05-25 10:04:01 +0200

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+
 sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`
 sudo chmod +x /usr/local/bin/cog
 
