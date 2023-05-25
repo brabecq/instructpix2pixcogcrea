6ef864f90e82a3562d5669cea311418dee9030c4
brabecq <92429709+brabecq@users.noreply.github.com>
2023-05-25 10:52:23 +0200

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -6,10 +6,12 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo 
 sudo apt update
 sudo apt install caddy
 
-caddy adapt
-
 sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`
 sudo chmod +x /usr/local/bin/cog
 
 sudo cog build -t instructpix2pixcogcrea
 sudo docker run -d -p 5000:5000 --gpus all instructpix2pixcogcrea
+
+caddy start
+caddy adapt
+caddy reload
